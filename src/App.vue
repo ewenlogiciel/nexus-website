4ec829cfc09e3385fed128cc6e477e15ce8bd4aa
ewenlogiciel <ewen.davanzo@gmail.com>
2026-01-27 13:39:29 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script setup></script>
 
 <template>
-  <h1>Nexus</h1>
-
+  <div class="page-container">
+    <h1>Nexus</h1>
+  </div>
 </template>
 
 <style scoped>
 
+:global(body) {
+  background-color: #000000;
+  color: white;
+  min-height: 100vh;
+  width: 100%;
+  margin: 0;
+}
 </style>
